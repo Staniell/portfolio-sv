05c6d51dd6c3457f327ffa10bb4a691549bb2dc4
staniell <giostaniell12@gmail.com>
2025-12-27 06:23:13 +0800

    init

---
+++ src/lib/index.ts
@@ -0,0 +1,7 @@
+// Components
+export { default as Hero } from './components/Hero.svelte';
+export { default as About } from './components/About.svelte';
+export { default as Experience } from './components/Experience.svelte';
+export { default as Skills } from './components/Skills.svelte';
+export { default as Contact } from './components/Contact.svelte';
+export { default as Navigation } from './components/Navigation.svelte';
